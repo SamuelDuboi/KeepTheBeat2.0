5d2207ad34c0d4f475a30ff5dce6ebf5c7d6c013
Corentin <job.corentin@outlook.fr>
2020-10-06 16:05:07 +0200

    Change color on beat effect

---
+++ Assets/Script/URPManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class URPManager : MonoBehaviour
+{
+    public static URPManager urpM;
+    Camera MainCam;
+    UniversalAdditionalCameraData cam;
+    bool locker;
+
+    void Start()
+    {
+        urpM = this;
+        MainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            if(locker == false)
+            StartCoroutine(ColorOnBeat(1, 1));
+        }
+        if (Input.GetKey("z"))
+        {
+            if (locker == false)
+                StartCoroutine(ColorOnBeat(2, 1));
+        }
+        if (Input.GetKey("e"))
+        {
+            if (locker == false)
+                StartCoroutine(ColorOnBeat(3, 1));
+        }
+        if (Input.GetKey("r"))
+        {
+            if (locker == false)
+                StartCoroutine(ColorOnBeat(4, 1));
+        }
+        if (Input.GetKey("t"))
+        {
+            if (locker == false)
+                StartCoroutine(ColorOnBeat(5, 1));
+        }
+        if (Input.GetKey("y"))
+        {
+            if (locker == false)
+                StartCoroutine(ColorOnBeat(6, 1));
+        }
+
+    }
+
+    public IEnumerator ColorOnBeat(int index, float time)
+    {
+        locker = true;
+        cam = MainCam.GetUniversalAdditionalCameraData();
+        cam.SetRenderer(index);
+        yield return new WaitForSeconds(time);
+        locker = false;
+        cam.SetRenderer(0);
+        
+    }
+}

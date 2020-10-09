832c71ddb796709d134add5b848caa8a7097c694
Corentin <job.corentin@outlook.fr>
2020-10-09 09:38:30 +0200

    WIp inté

---
+++ Assets/Script/FX/MeteorSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorSpawner : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(SpawnMeteor(Random.Range(0, 3f)));
+    }
+
+    IEnumerator SpawnMeteor(float time)
+    {
+        yield return new WaitForSeconds(time);
+        FXManager.fxm.InstantiateMeteor(transform);
+        StartCoroutine(SpawnMeteor(Random.Range(5, 10f)));
+    }
+}

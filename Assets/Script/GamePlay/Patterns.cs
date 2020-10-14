51745e65e6427db74775d73c4e382428e9b8298b
Dubois <s.dubois@rubika-edu.com>
2020-10-14 19:41:31 +0200

    modif tool 1/2

--- Assets/Script/GamePlay/Patterns.cs
+++ Assets/Script/GamePlay/Patterns.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPattern", menuName = "Pattern", order = 12)]
 public class Patterns : ScriptableObject
 {
-    public List<Vector2> ennemiesPositionForEditor;
-    public List<Vector2> ennemiesPosition;
+    public List<Vector3> ennemiesPositionForEditor;
+    public List<Vector3> ennemiesPosition;
     public int difficulty;
     public void Initialize()
     {
@@ -15,32 +15,32 @@ public class Patterns : ScriptableObject
         if (ennemiesPositionForEditor == null || ennemiesPositionForEditor.Count == 0)
         {
             //Initialize the list
-            ennemiesPositionForEditor = new List<Vector2>();
+            ennemiesPositionForEditor = new List<Vector3>();
 
         }
         if (ennemiesPosition == null || ennemiesPosition.Count == 0)
         {
             //Initialize the list
-            ennemiesPosition = new List<Vector2>();
+            ennemiesPosition = new List<Vector3>();
 
         }
 
     }
 
-    public void AddEnnemy(Vector2 position, int i, int x)
+    public void AddEnnemy(Vector3 position, int i, int x)
     {
         ennemiesPositionForEditor.Add(position);
-        ennemiesPosition.Add(new Vector2(i,x));
+        ennemiesPosition.Add(new Vector3(i,x,position.z));
     }
-    public void RemoveEnnemy(Vector2 position, int i, int x)
+    public void RemoveEnnemy(Vector3 position, int i, int x)
     {
         if (ennemiesPositionForEditor.Contains(position))
         {
             ennemiesPositionForEditor.Remove(position);
-            ennemiesPosition.Remove(new Vector2(i,x));
+            ennemiesPosition.Remove(new Vector3(i,x,position.z));
 
         }
         else
-            Debug.Log("ya un problème");
+            Debug.Log(position);
     }
 }

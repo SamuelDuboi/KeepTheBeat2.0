9b1777965ed8a89a99adae63ce1e45667944b674
Xanthin <x.paris@rubika-edu.com>
2020-10-16 10:38:49 +0200

    ADDING TMPro // Ajout Scoring Mort

---
+++ Assets/FloatingText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FloatingText : MonoBehaviour
+{
+    [SerializeField] private GameObject popPosition;
+    [SerializeField] private TextMeshPro popText;
+    [Range(0.3f, 3f)]
+    [SerializeField] private float deadFloat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TextMeshPro thisText = Instantiate(popText, popPosition.transform.position, Quaternion.identity);
+
+            Destroy(thisText, deadFloat);
+        }
+    }
+}

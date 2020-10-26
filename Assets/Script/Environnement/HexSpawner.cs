90bea4961a0ef5627a740ccaeeb98f6f8b7b028b
Corentin <job.corentin@outlook.fr>
2020-10-26 17:40:45 +0100

    Wip tunnel environnement

---
+++ Assets/Script/Environnement/HexSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexSpawner : MonoBehaviour
+{
+    public GameObject HexCircle;
+    public bool canSpawn;
+    void Start()
+    {
+        InvokeRepeating("SpawnCircle", 0.1f, 0.1f);
+    }
+
+    void Update()
+    {
+        if (canSpawn == true)
+        {
+
+        } 
+    }
+
+    void SpawnCircle()
+    {
+        Instantiate(HexCircle,transform.position,Quaternion.identity);
+    }
+}

1c57f935f454e04fee69e1081f42c3032e01ef8b
Xanthin <x.paris@rubika-edu.com>
2020-10-26 15:37:03 +0100

    Fix Musique

---
+++ Assets/delaySound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class delaySound : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine("WaitStart");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator WaitStart()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        audioSource.Play();
+    }
+}

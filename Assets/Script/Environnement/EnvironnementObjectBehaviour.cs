4bcc30adff9adbfa1a1eb2a25aaa157ddc71963a
Corentin <job.corentin@outlook.fr>
2020-10-27 16:36:49 +0100

    Environnement is WIPPPPP

---
+++ Assets/Script/Environnement/EnvironnementObjectBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnvironnementObjectBehaviour : MonoBehaviour
+{
+    Transform CamPos;
+    void Start()
+    {
+        CamPos = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if(transform.position.z < CamPos.transform.position.z - 20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

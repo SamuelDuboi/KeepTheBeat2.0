2314a9aed49cd1595fdb6a86673a2c894f979fb2
Corentin <job.corentin@outlook.fr>
2020-10-28 19:25:58 +0100

    Triangle Effect

--- Assets/Script/Environnement/ParticuleManager.cs
+++ Assets/Script/Environnement/ParticuleManager.cs
@@ -6,6 +6,14 @@ public class ParticuleManager :Singleton<ParticuleManager>
 {
     public List<GameObject> effectList = new List<GameObject>();
 
+    public void Start()
+    {
+        foreach (GameObject effect in effectList)
+        {
+            effect.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
     public void ActivateEffects()
     {
         foreach(GameObject effect in effectList)

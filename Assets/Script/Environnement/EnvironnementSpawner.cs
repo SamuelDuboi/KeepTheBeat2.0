ed7b3fe3dec66629ce69a4631da16ab426551f21
Corentin <job.corentin@outlook.fr>
2020-10-29 17:57:56 +0100

    New meteor swarm + wip du environnement manager

---
+++ Assets/Script/Environnement/EnvironnementSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironnementSpawner : MonoBehaviour
+{
+    public List<GameObject> objectToSpawn = new List<GameObject>();
+    public bool canSpawn;
+    public bool key = true;
+
+    void Update()
+    {
+        if (canSpawn == true && key == true)
+        {
+            StartCoroutine(Spawn());
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        key = false;
+        Instantiate(objectToSpawn[Random.Range(0,objectToSpawn.Count)], transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(Random.Range(3f, 7f));
+        key = true;
+    }
+}

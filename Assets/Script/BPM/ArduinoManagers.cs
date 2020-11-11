ce8ec75453f9d0cf56ab9927e9fc210bbb86a8f5
Xanthin <x.paris@rubika-edu.com>
2020-11-11 15:35:09 +0100

    PressAnyButtonAnim + Sound Validation

--- Assets/Script/BPM/ArduinoManagers.cs
+++ Assets/Script/BPM/ArduinoManagers.cs
@@ -31,15 +31,13 @@ public class ArduinoManagers : Singleton<ArduinoManagers>
 
     public Image fade;
 
+    [SerializeField] private GameObject SoundClic;
 
     private void Start()
     {
-        
-
         timerFloat = 20;// to enable timer only when started = true
         UduinoManager.Instance.OnDataReceived += DataReceived;
         DontDestroyOnLoad(this);
-
     }
 
     private void Update()
@@ -215,6 +213,8 @@ public class ArduinoManagers : Singleton<ArduinoManagers>
 
     IEnumerator FadeOut( bool tuto)
     {
+        SoundClic.GetComponent<AudioSource>().Play();
+
         for (float i = 255; i > 30; i--)
         {
             fade.color = new Color(0, 0, 0, i / 255);

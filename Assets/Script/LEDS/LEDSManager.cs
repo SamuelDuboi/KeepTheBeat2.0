066012bce5ff91d468fd98bdaf10ff01cd8f450e
Dubois <s.dubois@rubika-edu.com>
2020-11-12 14:34:48 +0100

    LEs LEds

--- Assets/Script/LEDS/LEDSManager.cs
+++ Assets/Script/LEDS/LEDSManager.cs
@@ -1,37 +1,38 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Uduino;
 public class LEDSManager : Singleton<LEDSManager>
 {
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
+        yield return new WaitForSeconds(2f);
         for (int i = 0; i < 6; i++)
         {
             if (i < 1)
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 255, 255, 0, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 255, 255, 0, 100);
             }
             else if (i>=1 && i < 2)
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 135, 255, 0, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 135, 255, 0, 100);
             }
             else if (i >= 2 && i < 3)
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 255, 0, 0, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 255, 0, 0, 100);
             }
             else if (i >= 3 && i < 4)
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 0, 255, 255, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 0, 255, 255, 100);
             }
             else if (i >= 4 && i < 5)
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 136, 0, 255, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 136, 0, 255, 100);
             }
             else
             {
-                UduinoManager.Instance.sendCommand("turnOnPixel", i, 248, 90, 205, 100);
+                UduinoManager.Instance.sendCommand("turnOn", i, 248, 90, 205, 100);
             }
         }
         
@@ -53,12 +54,12 @@ public class LEDSManager : Singleton<LEDSManager>
     {
         for (int i = row*6 ; i < 6 +row*6; i++)
         {
-            UduinoManager.Instance.sendCommand("changeBrightnessPixel", i,200);
+            UduinoManager.Instance.sendCommand("changeBrightness", i,200);
         }
         yield return new WaitForSeconds(0.5f);// this time will be change
         for (int i = row * 6; i < 6 + row * 6; i++)
         {
-            UduinoManager.Instance.sendCommand("changeBrightnessPixel", i, 100);
+            UduinoManager.Instance.sendCommand("changeBrightness", i, 100);
         }
     }
 }
